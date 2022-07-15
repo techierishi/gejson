e9bbc2212daa43066f066a717a01a56df0174e9e
Rishi <Rishikesh.Jha1@ibm.com>
2022-07-16 00:28:29 +0530

    react way, eye toggle

--- src/app/modal.js
+++ src/app/modal.js
@@ -1,52 +1,26 @@
 
 import { html } from 'htm/preact'
-import { useEffect } from 'preact/hooks'
+import { useEffect, useState } from 'preact/hooks'
 const Toastify = require('toastify-js')
+require('./eye-svg')
+
+const COMMIT_MESSAGE = 'Commit from Github JSON editor!'
 
 /* This code in transition phase from Vanilla JS to VDom */
 const Modal = function (props) {
 
-    const { settings, pathDetails } = props
+    const { pathDetails } = props
 
-    const fillSettings = (settings) => {
-        const { ghToken, ghName, ghEmail, ghCommitMessage } = settings || {}
-        if (ghToken) {
-            const ghTokenEl = document.getElementById("ghToken")
-            ghTokenEl.value = ghToken
-        }
-
-        if (ghName) {
-            const ghNameEl = document.getElementById("ghName")
-            ghNameEl.value = ghName
-        }
-
-        if (ghEmail) {
-            const ghEmailEl = document.getElementById("ghEmail")
-            ghEmailEl.value = ghEmail
-        }
-
-        if (ghCommitMessage) {
-            const ghCommitMessageEl = document.getElementById("ghCommitMessage")
-            ghCommitMessageEl.value = ghCommitMessage || 'Commit from Github JSON editor!'
-        }
-    }
+    const [tokenInputType, setTokenInputType] = useState("password")
+    const [settings, setSettings] = useState({
+        ghToken: props?.settings?.ghToken || '',
+        ghName: props?.settings?.ghName || '',
+        ghEmail: props?.settings?.ghEmail || '',
+        ghCommitMessage: props?.settings?.ghCommitMessage || COMMIT_MESSAGE
+    })
 
     const saveSettings = () => {
         const { ghHost } = pathDetails
-        const ghTokenEl = document.getElementById("ghToken")
-        const ghToken = ghTokenEl.value
-
-        const ghNameEl = document.getElementById("ghName")
-        const ghName = ghNameEl.value
-
-        const ghEmailEl = document.getElementById("ghEmail")
-        const ghEmail = ghEmailEl.value
-
-        const ghCommitMessageEl = document.getElementById("ghCommitMessage")
-        const ghCommitMessage = ghCommitMessageEl.value
-
-        const settings = { ghToken, ghName, ghEmail, ghCommitMessage }
-
         localStorage.setItem(ghHost, JSON.stringify(settings))
 
         Toastify({
@@ -66,49 +40,69 @@ const Modal = function (props) {
         document.querySelector('.modal').classList.remove('is-active');
     }
 
-    useEffect(() => {
-        fillSettings(settings)
-    })
-
+    const eyeClick = () => {
+        setTokenInputType((tokenInputType === "password" ? "text" : "password"))
+    }
     return html`
     <div class="modal" id="modal-settings">
         <div class="modal-background"></div>
         <div class="modal-card">
             <header class="modal-card-head">
-                <p class="modal-card-title">Modal title</p>
+                <p class="modal-card-title">Settings</p>
                 <button class="delete" aria-label="close" onclick=${closeModal}></button>
             </header>
             <section class="modal-card-body">
     
                 <div class="field">
                     <label class="label">Token</label>
-                    <div class="control has-icons-right">
-                        <input class="input" id="ghToken" type="password" placeholder="Gihub Token" />
     
-                        <span class="icon is-right">
-                            <i class="fas fa-check"></i>
-                        </span>
+                    <div class="control has-icons-right">
+                        <div class="token-wrapper">
+                            <input class="input eye-input" id="ghToken" type=${tokenInputType} placeholder="Gihub Token"
+                                value=${settings.ghToken} onchange=${(e) => {
+                                setSettings({ ...settings, ghToken: e.target.value })
+                                }} />
+
+                            <button class="button eye-btn" onclick=${eyeClick}>
+                                <span class="icon is-small">
+                                <eye-svg ></eye-svg>
+                                </span>
+                            </button>
+                            
+                        </div>
                     </div>
+    
                     <a href="https://docs.github.com/en/enterprise-server@3.4/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token"
                         target="_blank" rel="noopener noreferrer">How to create token?</a>
                 </div>
                 <div class="field">
                     <label class="label">Name</label>
                     <div class="control">
-                        <input class="input" id="ghName" type="text" placeholder="Name" />
+                        <input class="input" id="ghName" type="text" placeholder="Name" value=${settings.ghName}
+                            onchange=${(e) => {
+                            setSettings({ ...settings, ghName: e.target.value })
+                        }} />
                     </div>
                 </div>
     
                 <div class="field">
                     <label class="label">Email</label>
                     <div class="control">
-                        <input class="input" id="ghEmail" type="text" placeholder="Email" />
+                        <input class="input" id="ghEmail" type="text" placeholder="Email" value=${settings.ghEmail}
+                            onchange=${(e) => {
+                            setSettings({ ...settings, ghEmail: e.target.value })
+                        }}
+                        />
                     </div>
                 </div>
                 <div class="field">
                     <label class="label">Commit Message</label>
                     <div class="control">
-                        <input class="input" id="ghCommitMessage" type="text" placeholder="Commit Message" />
+                        <input class="input" id="ghCommitMessage" type="text" placeholder="Commit Message"
+                            value=${settings.ghCommitMessage} onchange=${(e) => {
+                            setSettings({ ...settings, ghCommitMessage: e.target.value || COMMIT_MESSAGE })
+                        }}
+                        />
                     </div>
                 </div>
     
